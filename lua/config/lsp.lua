a54e0bbb13b699523ae442662fd03a2086a35989
Koye <mail@koye64.com>
2025-09-25 03:37:01 -0700

    add lazydev.nvim

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,6 +1,9 @@
 vim.pack.add({
+  { src = 'https://github.com/folke/lazydev.nvim' },
   { src = 'https://github.com/neovim/nvim-lspconfig' },
 })
+
+require('lazydev').setup()
 
 vim.lsp.enable('lua_ls')
 vim.lsp.enable('rust_analyzer')
