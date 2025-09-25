155137e4a0a3b9b3b97c496b733d329f4c1ed955
Koye <mail@koye64.com>
2025-09-25 04:37:35 -0700

    enable built-in autocomplete

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,6 +1,6 @@
 vim.pack.add({
-  { src = 'https://github.com/folke/lazydev.nvim' },
   { src = 'https://github.com/neovim/nvim-lspconfig' },
+  { src = 'https://github.com/folke/lazydev.nvim' },
 })
 
 require('lazydev').setup()
@@ -9,4 +9,17 @@ vim.lsp.enable('lua_ls')
 vim.lsp.enable('rust_analyzer')
 vim.lsp.enable('texlab')
 vim.lsp.enable('tinymist')
+
+vim.api.nvim_create_autocommand('LspAttach', {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client:supports_method('textDocument/completion') then
+      vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+      vim.keymap.set('i', '<C-Space>', function()
+        vim.lsp.completion.get()
+      end)
+    end
+  end
+})
 
