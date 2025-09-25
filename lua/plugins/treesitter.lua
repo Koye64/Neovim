fd17e46a66a3f29b297c18bd8c9a7dfc6b1041e0
Koye <mail@koye64.com>
2025-09-25 05:15:22 -0700

    disable treesitter parsing for latex

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -10,6 +10,7 @@ require("nvim-treesitter.configs").setup({
   auto_install = true,
   highlight = {
     enable = true,
+    disable = { "latex" },
     additional_vim_regex_highlighting = false,
   },
   indent = {
