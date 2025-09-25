215ecc722f78e5113ea7ee015e5ae5e76e8e59d8
Koye <mail@koye64.com>
2025-09-25 04:37:17 -0700

    add autocommand for updating treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,11 @@
 vim.pack.add({
-  { src="https://github.com/nvim-treesitter/nvim-treesitter", version="master" }
+  {
+    src = "https://github.com/nvim-treesitter/nvim-treesitter",
+    version = "master"
+  }
 })
 
+---@diagnostic disable-next-line: missing-fields
 require("nvim-treesitter.configs").setup({
   auto_install = true,
   highlight = {
@@ -12,3 +16,21 @@ require("nvim-treesitter.configs").setup({
     enable = true,
   },
 })
+
+vim.api.nvim_create_autocmd('PackChanged', {
+  desc = 'Update nvim-treesitter',
+  group = vim.api.nvim_create_autogroup('nvim-treesitter-updater', { clear = true }),
+  callback = function(event)
+    if event.data.kind == 'update' then
+      vim.notify('nvim-treesitter updated, running TSUpdate...', vim.log.levels.INFO)
+      ---@diagnostic disable-next-line: param-type-mismatch
+      local ok = pcall(vim.cmd, 'TSUpdate')
+      if ok then
+        vim.notify('TSUpdate completed successfully', vim.log.levels.INFO)
+      else
+        vim.notify('TSUpdate unsuccessful', vim.log.levels.WARN)
+      end
+    end
+  end,
+})
+
