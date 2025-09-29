8442e986a8a379fbae79b588cfee2808162c6aff
Koye <mail@koye64.com>
2025-09-29 04:31:28 -0700

    enable lint on save, trigger lsp completion automatically

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -12,22 +12,31 @@ vim.lsp.enable('rust_analyzer')
 vim.lsp.enable('texlab')
 vim.lsp.enable('tinymist')
 
----@diagnostic disable: need-check-nil
 vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(cb_args)
-    local client = vim.lsp.get_client_by_id(cb_args.data.client_id)
+  group = vim.api.nvim_create_augroup('my.lsp', {}),
+  callback = function(args)
+    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+    -- Enable auto-completion. Note: Use CTRL-Y to select an item.
+    -- |complete_CTRL-Y|
     if client:supports_method('textDocument/completion') then
       vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
-      vim.lsp.completion.enable(
-        true,
-        client.id,
-        cb_args.buf,
-        { autotrigger = true }
-      )
-      vim.keymap.set('i', '<C-Space>', function()
-        vim.lsp.completion.get()
-      end)
+      -- Optional: trigger autocompletion on EVERY keypress. May be slow!
+      local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+      client.server_capabilities.completionProvider.triggerCharacters = chars
+      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
     end
-  end
-})
 
+    -- Auto-format ("lint") on save.
+    -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
+    if not client:supports_method('textDocument/willSaveWaitUntil')
+        and client:supports_method('textDocument/formatting') then
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+        buffer = args.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+        end,
+      })
+    end
+  end,
+})
