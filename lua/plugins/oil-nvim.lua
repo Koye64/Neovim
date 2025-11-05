2eb6230d9d66e87c43528350dff7ae0314e38566
Koye <mail@koye64.com>
2025-11-05 15:38:09 -0700

    remove extra options from oil

--- lua/plugins/oil-nvim.lua
+++ lua/plugins/oil-nvim.lua
@@ -2,13 +2,6 @@ vim.pack.add({
   { src = 'https://github.com/stevearc/oil.nvim' },
 })
 
-require("oil").setup({
-  columns = {
-    "icon",
-    "permissions",
-    "size",
-    "mime",
-  },
-})
+require("oil").setup()
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
